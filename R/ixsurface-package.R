fa610adfa01cf02524097795c0f58d1218782ea5
Chris Brantner <christopherbrantner@Christophers-MacBook-Air.local>
2026-03-21 11:21:16 -0500

    Fixed malformed regex, move demo.R Example.Rmd end explore.Rmd in root, fixed license duplication, fixed namespace

--- R/ixsurface-package.R
+++ R/ixsurface-package.R
@@ -2,7 +2,7 @@
 "_PACKAGE"
 
 #' @importFrom plotly plot_ly add_surface add_trace add_markers layout %>%
-#' @importFrom stats predict terms model.frame quantile rnorm runif median plogis binomial setNames
+#' @importFrom stats predict terms model.frame quantile rnorm runif median plogis binomial
 #' @importFrom utils combn head
 #' @importFrom grDevices col2rgb rgb colorRampPalette
 NULL
